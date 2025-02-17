95e603b26e0f7fc1a2555c1b2c7bad534fa06b7f
Henrique Ribeiro <yribeiro333br@gmail.com>
2025-02-17 18:16:43 -0300

    Adding exercise: class with variables

---
+++ apprenticeship/class8_exercise.py
@@ -0,0 +1,13 @@
+nome = 'Henrique'
+sobrenome = 'Ribeiro'
+idade = 17
+ano_nascimento = 2025 - idade
+maior_de_idade = idade >= 18
+altura = 1.72
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura em metros:', altura)
