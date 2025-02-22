904956d5c686205048b0e0422eade790dd809141
Henrique <yribeiro333br@gmail.com>
2025-02-22 00:01:06 -0300

    Adding basic Python classes: arithmetic operators, concatenation, repetition, operator precedence and introduction to f-strings

---
+++ apprenticeship/class10.py
@@ -0,0 +1,7 @@
+concatenacao = 'Henrique' + ' ' + 'Ribeiro'
+print(concatenacao)
+
+a_dez_vezes = 'A' * 10
+tres_vezes_henrique = 3 * 'Henrique'
+print(a_dez_vezes)
+print(tres_vezes_henrique)
---
+++ apprenticeship/class13.py
@@ -0,0 +1,13 @@
+nome = 'Henrique Ribeiro'
+altura = 1.72
+peso = 58
+imc = peso / altura ** 2
+
+"f-strings"
+linha_1 = f'{nome} tem {altura:.2f} de altura,'
+linha_2 = f'pesa {peso} quilos e seu IMC é'
+linha_3 = f'{imc:.2f}'
+
+print(linha_1)
+print(linha_2)
+print(linha_3)
