ceccb52596e6ffc3ca4a184e35076a8f0e69632e
Henrique <yribeiro333br@gmail.com>
2025-05-19 17:33:05 -0300

    🔹 Add: implement basic string interpolation example with variable definitions

---
+++ apprenticeship/class24.py
@@ -0,0 +1,12 @@
+"""
+Interpolação básica de strings
+s - string
+d e i - int
+f - float
+x e x - Hexadecimal (ABCDEF0123456789)
+"""
+
+nome = 'Henrique'
+preco = 1000.95897643
+variavel = ''
+print(variavel)
