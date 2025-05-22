9462d36e043bb26a450ef51682acea14b3b9c703
Henrique <yribeiro333br@gmail.com>
2025-05-22 00:12:56 -0300

    Fix: correct hexadecimal format to use uppercase notation

--- apprenticeship/class24.py
+++ apprenticeship/class24.py
@@ -10,4 +10,4 @@ nome = 'Henrique'
 preco = 1000.95897643
 variavel = '%s, o preço é R$%.2f' % (nome, preco)
 print(variavel)
-print('O hexadecimal de %d é %04x' % (15, 15))
+print('O hexadecimal de %d é %08X' % (1500, 1500))
