e8256cd1d300a79405fbc5c486fa8d3ceb9ccf28
Henrique <yribeiro333br@gmail.com>
2025-05-22 00:11:14 -0300

    🔹 Fix: update string formatting documentation and examples for clarity

--- apprenticeship/class25.py
+++ apprenticeship/class25.py
@@ -8,9 +8,10 @@ x ou X - Hexadecimal
 (Caractere)(><^)(quantidade)
 > - Esquerda
 < - Direita
+= - Força o número a aparecer antes dos zeros
 Sinal - + ou -
 Ex.: 0>-100,.1f
-Conversion flags - !r !s !a
+Conversion flags - !r !s !a __repr__ __str__
 """
 
 variavel = 'ABC'
@@ -18,4 +19,6 @@ print(f'{variavel}')
 print(f'{variavel: >10}')
 print(f'{variavel: <10}.')
 print(f'{variavel: ^10}.')
-print(f'{1000.4973648123746:,.1f}')
+print(f'{1000.4973648123746:0=+10,.1f}')
+print(f'O hexadecimal de 1500 é {1500:08X}')
+print(f'{variavel!a}')
