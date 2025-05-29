85b405ec41c77265de0bd2e04e3d00640cc43aba
Henrique <yribeiro333br@gmail.com>
2025-05-28 23:59:11 -0300

    Add: implement string slicing examples and documentation in class26.py

---
+++ apprenticeship/class26.py
@@ -0,0 +1,14 @@
+"""
+Fatiamento de strings
+012345678
+Olá Mundo
+-987654321
+Fatiamento [i:f:p] [::]
+Obs.: a função len retorna a quantidade
+de caracteres de str
+"""
+variavel = 'Olá mundo'
+print(variavel[4:])
+print(variavel[2])
+print(variavel[-4:-2])
+print(len(variavel))
