2d23024644f7a3a2dc00e30ff7742d30802de920
Henrique <yribeiro333br@gmail.com>
2025-06-20 18:14:33 -0300

    Fix: correct variable names and logic in password verification exercise

--- logical-challenges/challenge8.py
+++ logical-challenges/challenge8.py
@@ -1,18 +1,17 @@
 password = "1234"
-tentative = 0
-limit_tentatives = 3
+tentativas = 0
+limite_tentativas = 3
 
-while tentative < limit_tentatives:
-    input('Digite a senha:')
+while tentativas < limite_tentativas:
+    entrada = input('Digite a senha:')
     
-    if tentative == password:
+    if entrada == password:
         print("Acesso liberado")
         break
     else:
-        tentative =+ 1
-        restant = limit_tentatives - tentative
-        if restant > 0:
-            print(f"Senha incorreta. Tentativas restantes: {limit_tentatives - tentative}")
-        
-else:
-        print("Acesso bloqueado")
+        tentativas += 1
+        restantes = limite_tentativas - tentativas
+        if restantes > 0:
+            print(f"Senha incorreta. Tentativas restantes: {restantes}")       
+        else:
+            print("Acesso bloqueado")
