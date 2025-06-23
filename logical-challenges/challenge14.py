fb926edd34aca6f47a5515aa66b0e3dd855191a8
Henrique <yribeiro333br@gmail.com>
2025-06-23 19:06:20 -0300

    Add: implement student registration and grade calculation in challenge14.py

---
+++ logical-challenges/challenge14.py
@@ -0,0 +1,19 @@
+quantidade = int(input("Quantos alunos deseja cadastrar: "))
+
+for i in range(quantidade):
+    nome = input("Nome do aluno: ")
+    notas = []
+
+resumo = []
+
+for i in range(quantidade):
+    nota = float(input(f"Digite a nota {i}: "))
+    notas.append(nota)
+        
+media = sum(notas) / len(notas)
+
+resumo.append((nome, media))
+
+print("\nResumo final:")
+for nome in resumo:
+    print(f"{nome[0]}: média {nome[1]:.1f}")
